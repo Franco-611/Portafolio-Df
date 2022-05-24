76942bce66dfae3340bd5ecf087032ad8d5954c8
franco611 <62269078+franco611@users.noreply.github.com>
2022-05-24 00:25:36 -0600

    Avances proyectos

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,23 @@ function App() {
   return (
     <div className="App">
       <text className='nombre'>Diego Franco</text>
-      <div className='otro'>
-        ¿Que lenguajes se?
+      <div className='descrip'>
+        <text className='text'>Sobre mí</text>
+        <div className='yo'>
+          Soy un estudiante de la Universidad del Valle de Guatemala, actualemnte curso
+          tercer año de la Ingeniería en Ciencias de la Computación y Tecnologías de la Información. 
+          He desempeñado diferentes proyectos web tanto backend como frontend, manejo bases de 
+          datos y desarrollo de videojuegos.
+          <br/>
+          <br/>
+          Me considero una persona bastante dedicada a la que le gusta aprovechar el tiempo al máximo. 
+          En mis timpos libres suelo practicar deportes especialmente el balonmano, en el cual me he enfocado los 
+          ultimos 10 años de mi vida.
+        </div>
+      </div>
+      <div className='TitProyectos'/>
+      <div className='PROY'>
+        PROYECTOS..........
       </div>
     </div>
   )
